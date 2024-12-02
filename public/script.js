fbfb8c1e99f33877cedc207d2a9253d0a56c4af2
orsaadi <orsaadi9@gmail.com>
2024-12-02 18:26:24 +0200

    script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('wss://project-ideas-website-szhh.vercel.app');
+const socket = new WebSocket('ws://localhost:8080'');
 
 
 const nameInput = document.getElementById('name');
